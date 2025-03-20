e61ea4b0a9c88480210f0ce892dd11277e84403a
Bruno Casé <bruno.case45@gmail.com>
2025-03-20 17:15:02 -0300

    jwt-token

--- src/main/java/com/example/Atlas/model/Administrador.java
+++ src/main/java/com/example/Atlas/model/Administrador.java
@@ -24,4 +24,28 @@ public class Administrador extends Users{
     }
 
 
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
 }
--- src/main/java/com/example/Atlas/model/enums/ProjectStatus.java
+++ src/main/java/com/example/Atlas/model/enums/ProjectStatus.java
@@ -2,9 +2,13 @@ package com.example.Atlas.model.enums;
 
 public enum ProjectStatus {
 
-    AGUARDANDO_ANALISE_PRELIMINAR,
-    EM_ANALISE,
-    PROJETO_RECUSADO,
-    EM_ANDAMENTO,
-    FINALIZADO;
+    AGUARDANDO_ANALISE_PRELIMINAR("aguardando analise preliminar"),
+    EM_ANALISE("em analise"),
+    PROJETO_RECUSADO("projeto recusado"),
+    EM_ANDAMENTO("em andamento"),
+    FINALIZADO("finalizado");
+
+
+    ProjectStatus(String s) {
+    }
 }
