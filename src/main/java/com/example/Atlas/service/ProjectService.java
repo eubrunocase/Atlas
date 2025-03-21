76d36fdcbed87d8356e4c6a8a7b2e3727d42e8c2
Bruno Casé <bruno.case45@gmail.com>
2025-03-21 15:29:23 -0300

    readme

--- src/main/java/com/example/Atlas/service/ProjectService.java
+++ src/main/java/com/example/Atlas/service/ProjectService.java
@@ -3,6 +3,7 @@ package com.example.Atlas.service;
 import com.example.Atlas.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ProjectService extends BaseService<Project> {
@@ -10,4 +11,5 @@ public class ProjectService extends BaseService<Project> {
     public ProjectService(JpaRepository<Project, Long> repository) {
         super(repository);
     }
+
 }
